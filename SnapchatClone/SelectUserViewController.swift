e4576b1ea00c11d272ae4745a8ba31eec993486b
Steven Sherry <affinityforapps@gmail.com>
2016-12-18 14:30:56 -0600

    import users from Firebase

--- SnapchatClone/SelectUserViewController.swift
+++ SnapchatClone/SelectUserViewController.swift
@@ -7,22 +7,46 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+
 
 class SelectUserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var users : [User] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
+        FIRDatabase.database().reference().child("users").observe(FIRDataEventType.childAdded, with: {(snapshot) in
+            print(snapshot)
+            let user = User()
+            let snapshotDictionary = snapshot.value as? NSDictionary
+            user.email = snapshotDictionary!["email"] as! String
+            user.uid = snapshot.key
+            
+            self.users.append(user)
+            
+            self.tableView.reloadData()
+        })
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return users.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.email
+        
+        return cell
     }
 }
 
---
+++ SnapchatClone/user.swift
@@ -0,0 +1,14 @@
+//
+//  user.swift
+//  SnapchatClone
+//
+//  Created by Steven Sherry on 12/18/16.
+//  Copyright © 2016 Affinity for Apps. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var email = ""
+    var uid = ""
+}
