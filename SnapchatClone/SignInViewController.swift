54da0b5ec50a4b3a99d272afa612be45809746e4
Steven Sherry <affinityforapps@gmail.com>
2016-12-18 01:51:06 -0600

    Created sign-in page and segue to SnapViewController. The instructor’s method of having a one-click sign-up and sign-in method did not work for me (maybe because I’m using a newer version of Xcode?), so instead of making the if statement != nil after creating the user, I made it == nil to perform segue after user creation instead of the functions performed by the instructor…we’ll see if this doesn’t bite me in the ass as I continue through this section.

---
+++ SnapchatClone/SignInViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SignInViewController.swift
+//  SnapchatClone
+//
+//  Created by Steven Sherry on 12/17/16.
+//  Copyright © 2016 Affinity for Apps. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func turnUpTapped(_ sender: Any) {
+        
+        //Attempt to sign in
+        
+        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+            print("We tried to sign in")
+            
+            //If user does not have an account create one
+            
+            if error != nil {
+                print("Hey we have an error:\(error)")
+                
+                FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                    print("We tried to create a user")
+                    
+                    //After account is created, sign the user in
+                    
+                    if error == nil {
+                        self.performSegue(withIdentifier: "signInSegue", sender: nil)
+                    } else {
+                        print("Unable to create user")
+                    }
+                })
+                
+            //If user does have an account, sign them in
+                
+            } else {
+                print("We Signed In")
+                self.performSegue(withIdentifier: "signInSegue", sender: nil)
+            }
+            
+        })
+        
+    }
+    
+}
---
+++ SnapchatClone/SnapsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SnapsViewController.swift
+//  SnapchatClone
+//
+//  Created by Steven Sherry on 12/18/16.
+//  Copyright © 2016 Affinity for Apps. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+
+
